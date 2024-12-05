054630368ef9d225d7c2c979fc2e35e1ee47cc7e
Yash <yashsaini18166@gmail.com>
2024-12-05 19:03:25 +0530

    GET Request && env config

--- src/server.ts
+++ src/server.ts
@@ -1,89 +1,3 @@
-// import express, { json } from "express";
-// import WebSocket, { WebSocketServer } from "ws";
-// import dotenv from "dotenv";
-// import { createClient } from "redis";
-
-// dotenv.config();
-// const port = process.env.PORT;
-// const app = express();
-// const httpServer = app.listen(port);
-// const wss = new WebSocketServer({ server: httpServer });
-// const client = createClient();
-// const subscriber = client.duplicate();
-// let latestResult: any = null;
-// let wsClients: any[] = [];
-
-// async function connect() {
-//   await client.connect();
-// }
-// connect();
-
-// wss.on("connection", function connection(ws) {
-//   wsClients.push(ws);
-//   if (latestResult) {
-//     ws.send(latestResult.replace(/[^\x20-\x7E]/g, ""));
-//   } else {
-//     ws.send("Waiting for results!");
-//   }
-
-//   ws.on("message", function message(data: any) {
-//     try {
-//       if (data) {
-//         ws.send("Data exits, calling");
-//         ws.send(JSON.stringify(latestResult));
-//       }
-//       const parsedData = JSON.parse(data);
-//       const { code, language, userId } = parsedData;
-//       client.lPush("submission", JSON.stringify({ code, language, userId }));
-//       ws.send(
-//         JSON.stringify({
-//           success: true,
-//           message: "Code received and added to the queue",
-//         })
-//       );
-//     } catch (error) {
-//       console.log("Error processing message:", error);
-//       ws.send(
-//         JSON.stringify({
-//           success: false,
-//           message: "Failed to process the code.",
-//         })
-//       );
-//     }
-//   });
-
-//   ws.on("close", () => {
-//     wsClients = wsClients.filter((client: any) => client != ws);
-//     console.log("Web Sockets disconnected!");
-//   });
-//   ws.on("error", (err) => console.log(err));
-// });
-
-// async function setup() {
-//   try {
-//     await subscriber.connect();
-//     subscriber.subscribe("channel1", (message) => {
-//       console.log(
-//         "Message Received on channel1 : ",
-//         message.replace(
-//           /\\u([0-9]|[a-fA-F])([0-9]|[a-fA-F])([0-9]|[a-fA-F])([0-9]|[a-fA-F])/g,
-//           ""
-//         )
-//       );
-//       latestResult = message;
-//       wsClients.forEach((client: any) => {
-//         if (client.readyState === WebSocket.OPEN) {
-//           client.send(latestResult.replace(/\\u([0-9a-fA-F]{4})|\\n/g, ""));
-//         }
-//       });
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// setup();
-
 import express from "express";
 import WebSocket, { WebSocketServer } from "ws";
 import dotenv from "dotenv";
@@ -104,6 +18,10 @@ async function connect() {
 connect();
 
 let OneuserId: any;
+
+app.get("/", (req, res) => {
+  res.send("Hello From CodeEditor-Server !");
+});
 
 wss.on("connection", function connection(ws) {
   console.log("A new WebSocket client connected!");
